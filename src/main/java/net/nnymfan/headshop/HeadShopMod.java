354e3e9f7eb390364d0507ce16749ad6e60ba895
nnymfan-byte <nnymfan@gmail.com>
2026-09-22 13:55:52 -0400

    Fix command permission API

--- src/main/java/net/nnymfan/headshop/HeadShopMod.java
+++ src/main/java/net/nnymfan/headshop/HeadShopMod.java
@@ -64,7 +64,7 @@ public class HeadShopMod implements ModInitializer {
 
     private static void registerCommands(CommandDispatcher<net.minecraft.server.command.ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("headreset")
-                .requires(source -> source.getPermissionLevel() >= 2)
+                .requires(source -> source.getEntity() instanceof ServerPlayerEntity p && p.hasPermissionLevel(2))
                 .executes(ctx -> {
                     STATE.reset();
                     for (ServerPlayerEntity p : ctx.getSource().getServer().getPlayerManager().getPlayerList()) {
